7fc8e9f057f5d0997dc675b2bb11744294da2e8e
renatodellosso <rddellosso4@gmail.com>
2025-08-06 15:38:53 -0400

    Fill out /cool page

--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -7,7 +7,18 @@ export default function Kewl() {
       Feel free to put links, images, whatever! 
       Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
       */}
-      😎😎😎
+      <div className="animate-bounce">😎😎😎</div>
+      <div className="w-1/3">
+        I once spent a week sharing a cabin with a six-foot snake! "Cabin" might
+        be a strong word - it was more a three-walled shack in the woods. It
+        happened at Boy Scout camp; we showed up on Sunday, walked over to our
+        "cabin," and - something moved! After much kerfuffle, we found the snake
+        slithering around the cabin. This new cabinmate was concerning, until an
+        older scout clarified that it was Casper, who'd lived there for years.
+        We left the snake alone and it returned the favor, though it did slither
+        behind my friend while he was reading. We think Casper's children have
+        spread to the other cabins.
+      </div>
     </div>
   );
 }

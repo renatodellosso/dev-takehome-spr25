3d6a7bbc30ce738c6dc4565a354290ed7f8c6eb9
Renato Dell'Osso <rddellosso4@gmail.com>
2025-08-14 17:15:36 -0400

    Allow running tests without a .env file

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -1,20 +1,23 @@
 import { MongoClient } from "mongodb";
 import { ItemRequest } from "./types/request";
 
-if (!process.env.MONGODB_URI) {
+// global.__MONGO_URI__ is set by jest-mongodb to separate each Jest worker's database
+const mongoUri =
+  (global as unknown as { __MONGO_URI__: string }).__MONGO_URI__ ??
+  process.env.MONGODB_URI;
+
+const dbName = process.env.DB_NAME || "test";
+
+if (!mongoUri) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-if (!process.env.DB_NAME) {
+if (!dbName) {
   throw new Error("Please define the DB_NAME environment variable");
 }
 
-const mongoClient = new MongoClient(
-  // global.__MONGO_URI__ is set by jest-mongodb to separate each Jest worker's database
-  (global as unknown as { __MONGO_URI__: string }).__MONGO_URI__ ??
-    process.env.MONGODB_URI
-);
-const db = mongoClient.db(process.env.DB_NAME);
+const mongoClient = new MongoClient(mongoUri);
+const db = mongoClient.db(dbName);
 
 const collections = {
   requests: db.collection<ItemRequest>("requests"),
